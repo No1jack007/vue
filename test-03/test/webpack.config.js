3cd110b24585522a37c99284f0b255486d4aed3b
zhangyufei <2322575520@qq.com>
2020-10-16 17:45:22 +0800

    vue

--- test-03/test/webpack.config.js
+++ test-03/test/webpack.config.js
@@ -3,13 +3,27 @@ module.exports = {
     entry: './src/main.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        publicPath: 'dist/'
     },
     module: {
         rules: [
+            //加载css组件
             {
                 test: /\.css$/,
-                use: ['style-loader','css-loader']
+                use: ['style-loader', 'css-loader']
+            },
+            //加载图片组件
+            {
+                test: /\.(png|jpg|gif|jpeg)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 13000
+                        }
+                    }
+                ]
             }
         ]
     }
