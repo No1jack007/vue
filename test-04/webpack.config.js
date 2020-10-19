73a61ff298f2982eca787555746dde378e6b83cf
zhangyufei <2322575520@qq.com>
2020-10-19 16:22:09 +0800

    vue

---
+++ test-04/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path'); //调用node.js中的路径
+module.exports = {
+    entry: './src/main.js', //需要打包的文件
+    output: {
+        path: path.resolve(__dirname, 'dist'), //指定生成的文件目录
+        filename: 'bundle.js',    //输入的文件名是什么，生成的文件名也是什么
+    },
+    module: {
+        rules: [
+            //加载css组件
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            //加载图片组件
+            {
+                test: /\.(png|jpg|gif|jpeg)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 13000,
+                            name: 'img/[name].[hash:8].[ext]'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    resolve: {
+        //别名
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js'
+        }
+    }
+}
