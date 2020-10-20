199dd9a6fe80a22292e9c5eb836aa76161834d65
zhangyufei <2322575520@qq.com>
2020-10-20 11:46:16 +0800

    vue

--- test-04/src/main.js
+++ test-04/src/main.js
@@ -6,3 +6,23 @@ const app=new Vue({
         message:'Hello Webpack'
     }
 })
+
+import App2 from './vue/app.js'
+
+const app2=new Vue({
+    el:'#app2',
+    template:'<App2/>',
+    components:{
+        App2
+    }
+})
+
+import App3 from './vue/app3.vue'
+
+const app3=new Vue({
+    el:'#app3',
+    template:'<App3/>',
+    components:{
+        App3
+    }
+})
