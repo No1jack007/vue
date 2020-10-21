13d3fe28e1f4a900b645c154ebe0c09a294a3ba8
zhangyufei <2322575520@qq.com>
2020-10-21 19:02:05 +0800

    vue

--- test-05/build/dev.config.js
+++ test-05/build/dev.config.js
@@ -1,7 +1,7 @@
-const webpackMerge=require('webpack-merge')
-const baseConfig=require('./base.config.js')
+const webpackMerge = require('webpack-merge')
+const baseConfig = require('./base.config.js')
 
-module.exports=webpackMerge(baseConfig,{
+module.exports = webpackMerge(baseConfig, {
     devServer: {
         contentBase: './dist',
         inline: true
